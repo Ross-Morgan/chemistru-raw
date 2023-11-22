4ea59612c8d4566eb5f5d96f87324247472436b4
Ross-Morgan <rmorgan512@protonmail.ch>
2023-11-22 10:56:05 +0000

    add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! # chemistru-raw
+//! 
+//! Raw elemental data loaded from JSON, with no data cleansing
+
 lazy_static::lazy_static! {
     pub static ref ELEMENTS: Vec<chemistru_elements::raw::RawElement> = serde_json::from_str(include_str!("../periodic-table-data/periodic-table.json")).unwrap();
 }
